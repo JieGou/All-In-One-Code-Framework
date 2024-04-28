a10140a03e1be3f01c4b25914136ef682c6ed172
陈杰 <jiegou04@gmail.com>
2024-04-28 17:40:06 +0800

    code format

--- Visual Studio 2010/CSWPFDataGridCustomSort/MainWindow.xaml.cs
+++ Visual Studio 2010/CSWPFDataGridCustomSort/MainWindow.xaml.cs
@@ -10,8 +10,8 @@ This source is subject to the Microsoft Public License.
 See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.
 All other rights reserved.
 
-THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 \***************************************************************************/
 
@@ -32,11 +32,12 @@ namespace CSWPFDataGridCustomSort
         }
 
         /// <summary>
-        /// Declare a custom sort class which implements IComparer interface 
+        /// Declare a custom sort class which implements IComparer interface
         /// </summary>
         internal class JobSort : IComparer
         {
             private bool _direction;
+
             public JobSort(bool direction)
             {
                 _direction = direction;
@@ -69,12 +70,16 @@ namespace CSWPFDataGridCustomSort
                 {
                     case "CTO":
                         return 1;
+
                     case "Manager":
                         return 2;
+
                     case "Leader":
                         return 3;
+
                     case "Member":
                         return 4;
+
                     default:
                         return 0;
                 }
@@ -83,32 +88,33 @@ namespace CSWPFDataGridCustomSort
 
         /// <summary>
         /// This Sorting event handler when you click the Column Header named "Job"
-        /// and sort "Job" column content by Job kind 
-        /// </summary>  
+        /// and sort "Job" column content by Job kind
+        /// </summary>
         private void WPF_DataGrid_Sorting(object sender, DataGridSortingEventArgs e)
         {
             // Check the sorted column which needs to implement the custom sort
-            if (e.Column.Header.Equals("Job"))
+            if (!e.Column.Header.Equals("Job"))
             {
-                // Get the ListCollectionView from the DataGrid
-                ListCollectionView view = (ListCollectionView)(CollectionViewSource.GetDefaultView(datagrid.ItemsSource));
+                return;
+            }
+            // Get the ListCollectionView from the DataGrid
+            ListCollectionView view = (ListCollectionView)(CollectionViewSource.GetDefaultView(datagrid.ItemsSource));
 
-                if (e.Column != null && e.Column.CanUserSort == true)
+            if (e.Column != null && e.Column.CanUserSort == true)
+            {
+                // Create an object to implement the custom sort
+                if (e.Column.SortDirection == ListSortDirection.Ascending)
                 {
-                    // Create an object to implement the custom sort 
-                    if (e.Column.SortDirection == ListSortDirection.Ascending)
-                    {
-                        view.CustomSort = new JobSort(false);
-                        e.Column.SortDirection = ListSortDirection.Descending;
-                    }
-                    else
-                    {
-                        view.CustomSort = new JobSort(true);
-                        e.Column.SortDirection = ListSortDirection.Ascending;
-                    }
-
-                    e.Handled = true;
+                    view.CustomSort = new JobSort(false);
+                    e.Column.SortDirection = ListSortDirection.Descending;
                 }
+                else
+                {
+                    view.CustomSort = new JobSort(true);
+                    e.Column.SortDirection = ListSortDirection.Ascending;
+                }
+
+                e.Handled = true;
             }
         }
     }
--- Visual Studio 2010/CSWPFListBoxValidation/MainWindow.xaml.cs
+++ Visual Studio 2010/CSWPFListBoxValidation/MainWindow.xaml.cs
@@ -2,18 +2,18 @@
  * Module Name:  MainWindow.xaml.cs
  * Project: CSWPFListBoxValidation
  * Copyright (c) Microsoft Corporation.
- * 
+ *
  * This file contains the C# code for the CSWPFListBoxValidation.
  * The sample demonstrates how to add validation to a ListBox, overriding the control
  * to contain a ValidationListener property, which can be bound to provide validation
  * using built in validation UI features in WPF.
- * 
+ *
  * This source is subject to the Microsoft Public License.
  * See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.
  * All other rights reserved.
- * 
- * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
- * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 \***************************************************************************/
 
@@ -26,20 +26,20 @@ namespace CSWPFListBoxValidation
 {
     public partial class MainWindow : Window
     {
-        // This is a property that is used to provide the validation information.  
+        // This is a property that is used to provide the validation information.
         // In the sample, it is part of the Window control hosting the overridden ListBox.
-        // It could be anyplace, and is linked to the overridden ValidatingListBox 
+        // It could be anyplace, and is linked to the overridden ValidatingListBox
         // via the ValidationListener property's binding, specified in the page XAML.
-        ObservableCollection<string> i = new ObservableCollection<string>();
+        private ObservableCollection<string> i = new ObservableCollection<string>();
 
-        public static readonly DependencyProperty ValidationProperty = 
+        public static readonly DependencyProperty ValidationProperty =
             DependencyProperty.Register("Validation", typeof(string), typeof(Window),
             new PropertyMetadata(""));
 
         public MainWindow()
         {
             InitializeComponent();
-            
+
             // For the sample, we just add 2 items to a list and set the Window's DataContext to the list.
             i.Add("Item 1");
             i.Add("Item 2");
@@ -52,7 +52,7 @@ namespace CSWPFListBoxValidation
 
         private void ValidatingListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // We also want to force validation anytime the list selection changes, since the 
+            // We also want to force validation anytime the list selection changes, since the
             // validation criteria involve selection status of list items
             Validate(sender as ValidatingListBox);
         }
@@ -73,27 +73,29 @@ namespace CSWPFListBoxValidation
         }
     }
 
-    // This class is simply a ListBox with one additional property, the ValidationListener property, added.  
+    // This class is simply a ListBox with one additional property, the ValidationListener property, added.
     // One could also use an existing property like Tag, but since this property is often required for other purposes,
     // it is better to add a specific property for the purpose of the validation.
-    public class ValidatingListBox : ListBox 
+    public class ValidatingListBox : ListBox
     {
         // This is the validation property listener for the overridden version of the ListBox.
         // The property is bound to any property, and is used to determine validation state for the ListBox itself.
-        // Note the XAML where this property is bound, and the associated ValidationRule, which uses 
+        // Note the XAML where this property is bound, and the associated ValidationRule, which uses
         // the ListBoxValidationRule class below.
-        public static readonly DependencyProperty ValidationListenerProperty = 
-            DependencyProperty.Register("ValidationListener", typeof(string),  typeof(ValidatingListBox), 
-            new PropertyMetadata(""));
+        public static readonly DependencyProperty ValidationListenerProperty =
+            DependencyProperty.Register("ValidationListener",
+                                        typeof(string),
+                                        typeof(ValidatingListBox),
+                                        new PropertyMetadata(""));
 
         public string ValidationListener
-        { 
-            get 
+        {
+            get
             {
-                return (string)GetValue(ValidationListenerProperty); 
+                return (string)GetValue(ValidationListenerProperty);
             }
-            set 
-            { 
+            set
+            {
                 SetValue(ValidationListenerProperty, value);
             }
         }
@@ -104,7 +106,7 @@ namespace CSWPFListBoxValidation
     {
         // In this case, we have a very simple validation.  If there is text in the Validation property,
         // we consider the validation failed.  If the Validation property is empty, then it succeeds.
-        // The text of the property is set when we call the Validate() function in the Window class, 
+        // The text of the property is set when we call the Validate() function in the Window class,
         // which uses its own logic to decide whether to put an error message in the Validation property or not.
         public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
         {
